dd18d694555364f00418a184748eafb395b9e71e
shasingh <sharandeep.singh@niceactimize.com>
2020-07-18 15:46:12 +0530

    linkedlist with main

--- DataStructures/src/main/java/linear/LinkedList.java
+++ DataStructures/src/main/java/linear/LinkedList.java
@@ -24,10 +24,16 @@ public class LinkedList {
 
     public static void printList(LinkedList list){
         Node currNode = list.head;
-        while(currNode.next!=null){
+        while(currNode!=null){
             System.out.print(currNode.data + "");
             currNode = currNode.next;
         }
         System.out.println();
+    }
+
+    public static void main(String args[]){
+        LinkedList ll = new LinkedList();
+        insert(ll,1);
+        printList(ll);
     }
 }
