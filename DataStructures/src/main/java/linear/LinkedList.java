84a87db438483f94b0e13d8a6d36063a31e875d5
shasingh <sharandeep.singh@niceactimize.com>
2020-07-18 15:39:32 +0530

    linkedlist

---
+++ DataStructures/src/main/java/linear/LinkedList.java
@@ -0,0 +1,33 @@
+package linear;
+
+public class LinkedList {
+
+    Node head;
+
+    public static void insert(LinkedList list,int data){
+
+        Node node = new Node(data);
+        node.next = null;
+
+        if(list.head == null){
+            list.head = node;
+        } else {
+            // assuming initially head is a last element in this
+            Node last = list.head;
+            while(last.next!=null){
+                last = last.next;
+            }
+
+            last.next = node;
+        }
+    }
+
+    public static void printList(LinkedList list){
+        Node currNode = list.head;
+        while(currNode.next!=null){
+            System.out.print(currNode.data + "");
+            currNode = currNode.next;
+        }
+        System.out.println();
+    }
+}
---
+++ DataStructures/src/main/java/linear/Node.java
@@ -0,0 +1,11 @@
+package linear;
+
+public class Node {
+    public int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
